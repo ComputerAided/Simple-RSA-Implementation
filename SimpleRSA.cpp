bace3db6427d150eb81c0b34c1bc53fad06322c1
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-11 13:02:20 -0600

    Update SimpleRSA.cpp

--- SimpleRSA.cpp
+++ SimpleRSA.cpp
@@ -1,1 +1,13 @@
+#include "SimpleRSA.h"
 
+simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p1), q(q1) {
+  
+}
+
+unsigned long simpleRSA::encode (char c) {
+  
+}
+
+char simpleRSA::decode (unsigned long c) {
+  
+}
