ae116ae28dd5393c29dcf599e1511b835968c2e6
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-11 12:59:23 -0600

    Update SimpleRSA.h

--- SimpleRSA.h
+++ SimpleRSA.h
@@ -1,1 +1,14 @@
+#ifndef SIMPLERSA_H
+#define SIMPLERSA_H
 
+class simpleRSA {
+  public:
+    simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1);
+    unsigned long encode(char c);
+    char decode (unsigned long c);
+  private:
+    unsigned long p, q, d, e, n, t;
+  protected:
+};
+
+#endif
