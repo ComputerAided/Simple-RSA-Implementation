488c09dcdf1f44fea305e0047cf1dd39aec752dc
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-11 21:40:28 -0600

    Function for modular exponentiation added

--- SimpleRSA.cpp
+++ SimpleRSA.cpp
@@ -5,9 +5,13 @@ simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p
 }
 
 unsigned long simpleRSA::encode (char c) {
-  
+
 }
 
 char simpleRSA::decode (unsigned long c) {
-  
+
+}
+
+unsigned long modExp (unsigned long base, unsigned long base, unsigned long pow) {
+
 }
