3ca9a7ec0b3a4f17f0582f2cbb58d6a0915fbe93
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-12 22:22:59 -0600

    Update simpleRSA::modExp to public for testing

--- SimpleRSA.h
+++ SimpleRSA.h
@@ -6,11 +6,12 @@ class simpleRSA {
     simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1);
     unsigned long encode(char c);
     char decode (unsigned long c);
-  private:
-    const unsigned long p, q;
-    unsigned long d, e, n, t;
-  protected:
     unsigned long modExp (unsigned long base, unsigned long exp, unsigned long mod);
+  private:
+    const unsigned long p, q, n;
+    unsigned long d, e, t;
+  protected:
+
 };
 
 #endif
