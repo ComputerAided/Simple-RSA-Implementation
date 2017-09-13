5273ae1876ecd4853b39abc2f32c85fd72863d26
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-12 18:49:45 -0600

    Add returns to compile. Working functions coming soon.

--- SimpleRSA.cpp
+++ SimpleRSA.cpp
@@ -1,17 +1,17 @@
 #include "SimpleRSA.h"
 
 simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p1), q(q1) {
-  
+
 }
 
 unsigned long simpleRSA::encode (char c) {
-
+	return 0;
 }
 
 char simpleRSA::decode (unsigned long c) {
-
+	return 0;
 }
 
-unsigned long modExp (unsigned long base, unsigned long base, unsigned long pow) {
-
+unsigned long modExp (unsigned long base, unsigned long pow, unsigned long mod) {
+	return 0;
 }
