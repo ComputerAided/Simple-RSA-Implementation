312734d5309c66bb26f49567dc870b9a2a509e4e
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-12 22:22:21 -0600

    change to include n in initializer and add mod exp function

--- SimpleRSA.cpp
+++ SimpleRSA.cpp
@@ -1,17 +1,29 @@
 #include "SimpleRSA.h"
 
-simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p1), q(q1) {
+simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p1), q(q1), n(p*q) {
 
 }
 
 unsigned long simpleRSA::encode (char c) {
-	return 0;
+  return 0;
 }
 
 char simpleRSA::decode (unsigned long c) {
-	return 0;
+  return 0;
 }
 
-unsigned long modExp (unsigned long base, unsigned long pow, unsigned long mod) {
-	return 0;
+unsigned long simpleRSA::modExp (unsigned long base, unsigned long pow, unsigned long mod) {
+  unsigned long remainder;
+  unsigned long x = 1;
+
+  while (pow != 0) {
+    remainder = pow % 2;
+    pow = pow/2;
+
+    if (remainder == 1)
+    x = (x * base) % mod;
+    base = (base * base) % mod; // New base equal b^2 % m
+  }
+
+  return x;
 }
