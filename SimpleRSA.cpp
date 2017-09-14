f929146e9c3f4b799f9cc909566fd81531d13417
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-14 17:51:02 -0600

    Update to calculate variables.

--- SimpleRSA.cpp
+++ SimpleRSA.cpp
@@ -1,7 +1,9 @@
 #include "SimpleRSA.h"
+#include <iostream>
 
-simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p1), q(q1), n(p*q) {
-
+simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p1), q(q1), e(e1), n(p*q), t((p-1)*(q-1)) {
+  //std::cout << "p, q: " << p << ", " << q << "\r\n" << "t, n, e:" << t << ", " << n << ", " << e <<std::endl;
+  d = modInverse(e, t);
 }
 
 unsigned long simpleRSA::encode (char c) {
@@ -10,7 +12,7 @@ unsigned long simpleRSA::encode (char c) {
 }
 
 char simpleRSA::decode (unsigned long c) {
-  unsigned long C = modexp(c, d, n);
+  unsigned long C = modExp(c, d, n);
   return C;
 }
 
@@ -24,8 +26,41 @@ unsigned long simpleRSA::modExp (unsigned long base, unsigned long pow, unsigned
 
     if (remainder == 1)
     x = (x * base) % mod;
-    base = (base * base) % mod; // New base equal b^2 % m
+    base = (base * base) % mod;
   }
 
   return x;
+}
+
+unsigned long simpleRSA::modInverse(int a, int m)
+{
+    unsigned long m0 = m, t, q;
+    unsigned long x0 = 0, x1 = 1;
+
+    if (m == 1)
+      return 0;
+
+    while (a > 1)
+    {
+        // q is quotient
+        q = a / m;
+
+        t = m;
+
+        // m is remainder now, process same as
+        // Euclid's algo
+        m = a % m, a = t;
+
+        t = x0;
+
+        x0 = x1 - q * x0;
+
+        x1 = t;
+    }
+
+    // Make x1 positive
+    if (x1 < 0)
+       x1 += m0;
+
+    return x1;
 }
