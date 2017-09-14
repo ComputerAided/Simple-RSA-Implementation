f7d055b85db7b605e3548341e33a21c41c2df580
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-14 17:51:44 -0600

    implement new function and change access to modInverse and modExp

--- SimpleRSA.h
+++ SimpleRSA.h
@@ -6,10 +6,12 @@ class simpleRSA {
     simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1);
     unsigned long encode(char c);
     char decode (unsigned long c);
+  protected:
+    unsigned long modInverse(int a, int m);
     unsigned long modExp (unsigned long base, unsigned long exp, unsigned long mod);
   private:
-    const unsigned long p, q, n t;
-    unsigned long d, e;
+    const unsigned long p, q, e, n, t;
+    unsigned long d;
   protected:
 
 };
