bde57746d0b0a95626285b6c7f9e48c038938bdb
ComputerAided <ComputerAided@users.noreply.github.com>
2017-09-18 10:50:56 -0600

    Update main.ino
    Initial Commit

--- main.ino
+++ main.ino
@@ -1,1 +1,18 @@
+#include <SimpleRSA.h>
 
+simpleRSA rsa(1619, 1523, 85);
+
+void setup () {
+  Serial.begin(115200);
+  
+  char c = 'w';
+  unsigned long encoded = rsa.encode(c);
+  char decoded = rsa.decode(encoded);
+  
+  String out = "starting character: " + c + " encoded character:" + encoded + " decoded character: " + decoded;
+  Serial.println(out);
+}
+
+void loop () {
+  while (1);
+}
