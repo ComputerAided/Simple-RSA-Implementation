beda0736db0df66db72ff13234760dc64b4f9d30
ComputerAided <ComputerAided@users.noreply.github.com>
2017-10-25 11:02:39 -0600

    add new comment for clarification

--- SimpleRSA.cpp
+++ SimpleRSA.cpp
@@ -1,6 +1,6 @@
 #include "SimpleRSA.h"
 
-#include <iostream>
+#include <iostream> // Only needed for computers
 
 simpleRSA::simpleRSA(unsigned long p1, unsigned long q1, unsigned long e1) : p(p1), q(q1), e(e1), n(p*q), t((p-1)*(q-1)) {
   d = modInverse(e, t);
